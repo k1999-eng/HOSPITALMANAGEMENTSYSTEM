75d2bdc0f211a224bd64171e9b001f1457b0af0b
Krishnapriya-git <55920502+k1999-eng@users.noreply.github.com>
2021-12-19 10:25:32 +0530

    doctor view updated

--- HOSPITALMANAGEMENTSYSTEM/Controllers/DoctorController.cs
+++ HOSPITALMANAGEMENTSYSTEM/Controllers/DoctorController.cs
@@ -13,5 +13,29 @@ namespace HOSPITALMANAGEMENTSYSTEM.Controllers
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Appointments()
+        {
+            return View();
+        }
+        public ActionResult Patients()
+        {
+            return View();
+        }
     }
 }
